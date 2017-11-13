6338d5e3e01a0512720ff0ed82a2dac04c9dae71
Sabit Huraira <sabitzhabit@gmail.com>
2017-11-13 12:13:39 +0700

    setting vue model to mfd

--- protected/views/mfd/form.php
+++ protected/views/mfd/form.php
@@ -1,152 +1,156 @@
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <h3 class="box-title">Master File Desa</h3>
-    </div>
-    <!-- /.box-header -->
-    <!-- form start -->
-    <form role="form">
-
-        <div class="row row-print">
-            <div class="col-md-2">
-                <div class="form-group">
-                    <label>Kode Provinsi</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-md-10">
-                <div class="form-group">
-                    <label>Nama Provinsi</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
+<div id="mfdform_tag">
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title">Master File Desa</h3>
         </div>
+        <!-- /.box-header -->
+        <!-- form start -->
+        <form role="form">
 
-        <div class="row row-print">
-            <div class="col-md-2">
-                <div class="form-group">
-                    <label>Kode Kabupaten</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-md-10">
-                <div class="form-group">
-                    <label>Nama Kabupaten</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-        </div>
-
-
-        <div class="row row-print">
-            <div class="col-md-2">
-                <div class="form-group">
-                    <label>Kode Kecamatan</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-md-10">
-                <div class="form-group">
-                    <label>Nama Kecamatan</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-        </div>
-
-
-        <div class="row row-print">
-            <div class="col-md-2">
-                <div class="form-group">
-                    <label>Kode Desa</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-md-10">
-                <div class="form-group">
-                    <label>Nama Desa</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-        </div>
-
-
-        <div class="row row-print">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Blok Sensus (contoh: 001B, 005P)</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-        </div>
-
-
-
-        <div class="row row-print">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>Jumlah KK</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>Jumlah BSBTT/BSTTK</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-        </div>
-
-
-        <div class="row row-print">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Muatan Dominan</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-        </div>
-
-        <div class="row row-print">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>Ruta Biasa</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>Ruta Khusus</label>
-                    <input type="text" class="form-control">
-                </div>
-            </div>
-        </div>
+            <input type="hidden" v-model="idnya">
             
+            <div class="row row-print">
+                <div class="col-md-2">
+                    <div class="form-group">
+                        <label>Kode Provinsi</label>
+                        <input v-model="prov_no" type="text" class="form-control">
+                    </div>
+                </div>
 
-        <div class="row row-print">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>ART Laki-laki</label>
-                    <input type="text" class="form-control">
+                <div class="col-md-10">
+                    <div class="form-group">
+                        <label>Nama Provinsi</label>
+                        <input v-model="prov_nama" type="text" class="form-control">
+                    </div>
                 </div>
             </div>
 
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>ART Perempuan</label>
-                    <input type="text" class="form-control">
+            <div class="row row-print">
+                <div class="col-md-2">
+                    <div class="form-group">
+                        <label>Kode Kabupaten</label>
+                        <input v-model="kab_no" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-md-10">
+                    <div class="form-group">
+                        <label>Nama Kabupaten</label>
+                        <input v-model="kab_nama" type="text" class="form-control">
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="box-footer">
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </form>
+
+            <div class="row row-print">
+                <div class="col-md-2">
+                    <div class="form-group">
+                        <label>Kode Kecamatan</label>
+                        <input v-model="kec_no" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-md-10">
+                    <div class="form-group">
+                        <label>Nama Kecamatan</label>
+                        <input v-model="kec_nama" type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row row-print">
+                <div class="col-md-2">
+                    <div class="form-group">
+                        <label>Kode Desa</label>
+                        <input v-model="desa_no" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-md-10">
+                    <div class="form-group">
+                        <label>Nama Desa</label>
+                        <input v-model="desa_nama" type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row row-print">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label>Blok Sensus (contoh: 001B, 005P)</label>
+                        <input v-model="blok_sensus" type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="row row-print">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Jumlah KK</label>
+                        <input v-model="kk" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Jumlah BSBTT/BSTTK</label>
+                        <input v-model="bsbtt" type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row row-print">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label>Muatan Dominan</label>
+                        <input v-model="muatan_dominan" type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row row-print">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Ruta Biasa</label>
+                        <input v-model="ruta_biasa" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Ruta Khusus</label>
+                        <input v-model="ruta_khusus" type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+                
+
+            <div class="row row-print">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>ART Laki-laki</label>
+                        <input v-model="art_laki" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>ART Perempuan</label>
+                        <input v-model="art_perempuan" type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+            <div class="box-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
 </div>
 
 
---
+++ themes/adminlte/dist/js/vue_page/mfd_form.js
@@ -0,0 +1,21 @@
+var vm = new Vue({  
+    el: "#mfdform_tag",
+    data: {
+        art_laki: 0,
+        art_perempuan: 0,
+        blok_sensus: '',
+        bsbtt: 0,
+        desa_nama: '',
+        desa_no: '',
+        kab_nama: '',
+        kab_no: '',
+        kec_nama: '',
+        kec_no: '',
+        kk: 0,
+        muatan_dominan: '',
+        prov_nama: '',
+        prov_no: '',
+        ruta_biasa: 0,
+        ruta_khusus: 0
+    },
+});
