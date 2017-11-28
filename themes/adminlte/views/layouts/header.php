988adf120d6948f18ea020ea4cf70b56f80309b3
Sabit Huraira <sabitzhabit@gmail.com>
2017-11-28 14:02:53 +0700

    hidden identify

--- themes/adminlte/views/layouts/header.php
+++ themes/adminlte/views/layouts/header.php
@@ -2,10 +2,8 @@
 
     <!-- Logo -->
     <a href="index2.html" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>S</b>KT</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>SIMKET</b></span>
+      <!-- <span class="logo-mini"><b>S</b>KT</span>
+      <span class="logo-lg"><b>SIMKET</b></span> -->
     </a>
 
     <!-- Header Navbar: style can be found in header.less -->
