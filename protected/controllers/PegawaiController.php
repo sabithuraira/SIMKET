b3b6687ef17ba4d689eca06df8c448eb1bcd2797
Sabit Huraira <sabitzhabit@gmail.com>
2017-11-28 17:41:53 +0700

    set palembang only 2

--- protected/controllers/PegawaiController.php
+++ protected/controllers/PegawaiController.php
@@ -126,9 +126,10 @@ class PegawaiController extends Controller
 	{
 		$model=new Pegawai('search');
 		$model->unsetAttributes();  // clear any default values
-		$model->unit_kerja = 26;
 		if(isset($_GET['Pegawai']))
 			$model->attributes=$_GET['Pegawai'];
+
+		$model->unit_kerja = 26;
 
 		$this->render('admin',array(
 			'model'=>$model,
