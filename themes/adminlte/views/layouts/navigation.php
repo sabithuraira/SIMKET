bdd6df187146c4da48249e20394c6ef8aa1dbf6a
Sabit Huraira <sabitzhabit@gmail.com>
2017-12-28 22:03:41 +0700

    fix production navigation

--- themes/adminlte/views/layouts/navigation.php
+++ themes/adminlte/views/layouts/navigation.php
@@ -43,32 +43,17 @@
         <li class="treeview">
           <a href="#">
             <i class="fa fa-line-chart"></i>
-            <span>Peringkat</span>
+            <span>Peringkat dan Nilai</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
           <?php 
-              echo '<li><a href="'.Yii::app()->createUrl('site/peringkat').'"><i class="fa fa fa-circle-o"></i> Kabupaten/Kota</a></li>';
-              echo '<li><a href="'.Yii::app()->createUrl('site/peringkat_month').'"><i class="fa fa fa-circle-o"></i> Kabupaten/Kota Bulanan</a></li>';
-            ?>
-          </ul>
-        </li>
+              echo '<li><a href="'.Yii::app()->createUrl('site/peringkat').'"><i class="fa fa fa-circle-o"></i> Peringkat Tahunan</a></li>';
+              echo '<li><a href="'.Yii::app()->createUrl('site/peringkat_month').'"><i class="fa fa fa-circle-o"></i> Peringkat Bulanan</a></li>';
 
-
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-bar-chart"></i>
-            <span>Nilai Kab/Kota</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <?php 
-              $list_kabupaten=HelpMe::getListKabupaten();
-
+              $list_kabupaten=HelpMe::getListKabupaten();              
               foreach($list_kabupaten as $row){
                 echo '<li><a href="'.Yii::app()->createUrl('report/kabupaten',array('id'=> $row['id'])).'"><i class="fa fa fa-circle-o"></i> '.$row['label'].'</a></li>';
               }
@@ -111,22 +96,17 @@
           </ul>
         </li>
 
-        <?php if(Yii::app()->user->getUnitKerja()==26){ ?>
         
-          <li class="header">TUGAS DAN DINAS LUAR</li>
+        <li class="header">TUGAS DAN DINAS LUAR</li>
         <li><a href="<?php echo Yii::app()->createUrl('jadwalTugas/create'); ?>"><i class="fa fa-bicycle"></i><span> Buat Surat Tugas</span></a></li>
         <li><a href="<?php echo Yii::app()->createUrl('jadwalTugas/index'); ?>"><i class="fa fa-bicycle"></i><span> Manajemen Surat Tugas</span></a></li>
         <li><a href="<?php echo Yii::app()->createUrl('jadwalTugas/calendar'); ?>"><i class="fa fa-calendar"></i><span> Kalender Tugas dan DL</span></a></li>
         <li><a href="<?php echo Yii::app()->createUrl('jadwalTugas/single_calendar'); ?>"><i class="fa fa-calendar-plus-o"></i><span> Kalender Pegawai</span></a></li>
         
-        <?php
-          }else if(Yii::app()->user->getUnitKerja()==27){
-        ?>
         <li class="header">WILAYAH</li>
           <?php 
             echo '<li><a href="'.Yii::app()->createUrl('mfd/index').'"><i class="fa fa-map-o"></i> Wilayah Sumatera Selatan</a></li>';
           ?>
-        <?php } ?>
         <li class="header">LAINNYA</li>
         <li class="treeview">
           <a href="#">
@@ -141,8 +121,8 @@
               echo '<li><a href="'.Yii::app()->createUrl('user/index').'"><i class="fa fa-user"></i> User</a></li>';
               echo '<li><a href="'.Yii::app()->createUrl('pegawai/index').'"><i class="fa fa-user-plus"></i> Pegawai</a></li>';
               echo '<li><a href="'.Yii::app()->createUrl('unitdaerah/index').'"><i class="fa  fa-bookmark-o"></i> Unit Kerja Kab/Kota</a></li>';
-              // echo '<li><a href="'.Yii::app()->createUrl('kegiatan/index').'"><i class="fa fa-cube"></i> Kegiatan</a></li>';
-              // echo '<li><a href="'.Yii::app()->createUrl('unitkerja/index').'"><i class="fa fa-building-o"></i> Unit Kerja</a></li>';
+              echo '<li><a href="'.Yii::app()->createUrl('kegiatan/index').'"><i class="fa fa-cube"></i> Kegiatan</a></li>';
+              echo '<li><a href="'.Yii::app()->createUrl('unitkerja/index').'"><i class="fa fa-building-o"></i> Unit Kerja</a></li>';
             ?>
           </ul>
         </li>
