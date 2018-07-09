841a55c13019f1c4d18ceb331ee0e4f2935a295e
Sabit Huraira <sabitzhabit@gmail.com>
2018-07-09 13:10:49 +0700

    1604 - fix guess only access database mitra

--- themes/adminlte/views/layouts/navigation_1604.php
+++ themes/adminlte/views/layouts/navigation_1604.php
@@ -6,22 +6,25 @@
 
         <?php if(!Yii::app()->user->isGuest){ ?>
           <?php
-            echo '<li><a href="'.Yii::app()->createUrl('mitrabps/index').'"><i class="fa fa-user-plus"></i> Kelola Petugas Mitra</a></li>';
-            echo '<li><a href="'.Yii::app()->createUrl('pegawai/index').'"><i class="fa fa-user-plus"></i> Kelola Petugas Organik</a></li>';
-            echo '<li><a href="'.Yii::app()->createUrl('pertanyaan/index').'"><i class="fa fa-user-plus"></i> Kelola Pertanyaan Penilaian</a></li>';
+            if(Yii::app()->user->id != 'guess') 
+            {
+              echo '<li><a href="'.Yii::app()->createUrl('mitrabps/index').'"><i class="fa fa-user-plus"></i> Kelola Petugas Mitra</a></li>';
+              echo '<li><a href="'.Yii::app()->createUrl('pegawai/index').'"><i class="fa fa-user-plus"></i> Kelola Petugas Organik</a></li>';
+              echo '<li><a href="'.Yii::app()->createUrl('pertanyaan/index').'"><i class="fa fa-user-plus"></i> Kelola Pertanyaan Penilaian</a></li>';
 
-            echo '<li><a href="'.Yii::app()->createUrl('kegiatan_mitra').'"><i class="fa fa-bicycle"></i><span> Kelola Penilaian Daftar Kegiatan</span></a></li>';
+              echo '<li><a href="'.Yii::app()->createUrl('kegiatan_mitra').'"><i class="fa fa-bicycle"></i><span> Kelola Penilaian Daftar Kegiatan</span></a></li>';
 
-            echo '<li class="treeview"><a href="#"><i class="fa fa-circle-o"></i> Rapor Penilaian Kinerja
-              <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-            </a>';
+              echo '<li class="treeview"><a href="#"><i class="fa fa-circle-o"></i> Rapor Penilaian Kinerja
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>';
 
-            echo '<ul class="treeview-menu">';
-              echo '<li><a href="'.Yii::app()->createUrl('pegawai/rapor').'"><i class="fa fa-user-plus"></i> Pegawai</a></li>';
-              echo '<li><a href="'.Yii::app()->createUrl('mitrabps/rapor').'"><i class="fa fa-user-plus"></i> Mitra BPS</a></li>';
-            echo '</ul></li>';
+              echo '<ul class="treeview-menu">';
+                echo '<li><a href="'.Yii::app()->createUrl('pegawai/rapor').'"><i class="fa fa-user-plus"></i> Pegawai</a></li>';
+                echo '<li><a href="'.Yii::app()->createUrl('mitrabps/rapor').'"><i class="fa fa-user-plus"></i> Mitra BPS</a></li>';
+              echo '</ul></li>';
+            }
 
             echo '<li><a href="'.Yii::app()->createUrl('mitrabps/dbase').'"><i class="fa fa-user-plus"></i> Database Mitra BPS Kab/Kota</a></li>';            
           ?>
